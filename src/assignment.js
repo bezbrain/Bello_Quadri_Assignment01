262c85b0e1cb0dfee129ac30f9022dd7027fe6d6
Replit user <>
2023-08-24 21:29:42 +0000

    Complete task one and task two

--- src/assignment.js
+++ src/assignment.js
@@ -15,12 +15,15 @@ const assignment = {};
  * @returns number the sum of the numbers from 1 to destination
  */
 function sumOfNumbersTo(destination) {
-    let sum = 0;
-    return sum
+  let sum = 0;
+  for (let i = 1; i <= destination; i++) {
+    sum += i
+  }
+  return sum
 }
+sumOfNumbersTo(10)
 
-
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -33,23 +36,31 @@ function sumOfNumbersTo(destination) {
  * @returns the object containing count, sum, arrayOfEvenNumbers from 1 to destination
  */
 function countEvenNumbersWithin(destination) {
-    // Write your code here
-    // get the number from 1 to destination
-    let sum = 0;
-    let count = 0;
-    let arrayOfEvenNumbers = [];
+  // Write your code here
+  // get the number from 1 to destination
+  let sum = 0;
+  let count = 0;
+  let arrayOfEvenNumbers = [];
+  for (let i = 1; i <= destination; i++) {
+    if(i % 2 === 0) {
+      sum += i // Sum all even numbers
+      arrayOfEvenNumbers.push(i) // Push all even numbers into the array above
+    }
+  }
+      count = arrayOfEvenNumbers.length // Pass in the number of even numbers involved
 
-    return {
-        // property value shorthand
-        // when the property name and the value name are the same
-        // you can just write the property name in your object
-        count,
-        sum,
-        arrayOfEvenNumbers
-    };
+  return {
+    // property value shorthand
+    // when the property name and the value name are the same
+    // you can just write the property name in your object
+    count,
+    sum,
+    arrayOfEvenNumbers
+  };
 }
+countEvenNumbersWithin(10)
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
@@ -67,9 +78,9 @@ function countEvenNumbersWithin(destination) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    let result = [];
+  let result = [];
 
-    return result;
+  return result;
 }
 
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
